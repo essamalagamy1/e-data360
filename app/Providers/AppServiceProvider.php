4465ccef2103ff60da949c3f7b8caf54c19fc16d
Essam Hamdy AL-agamy <20912018100106@fci.zu.edu.eg>
2025-12-18 16:29:47 +0200

    feat: add Laravel Pulse for application monitoring and set up authorization.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use App\View\Composers\AppComposer;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,8 +23,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if (!$this->app->runningInConsole()) {
+        if (! $this->app->runningInConsole()) {
             View::composer(['components.navbar', 'components.footer', 'components.layouts.app'], AppComposer::class);
         }
+
+        Gate::define('viewPulse', function (User $user) {
+            return $user->email == 'superadmin@admin.com';
+        });
     }
 }
